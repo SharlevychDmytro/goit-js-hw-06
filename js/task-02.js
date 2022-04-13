783c019b84353df29c32c01b7b548f06c2c05796
Sharlevych Dmytro <sharlevychdmytro@gmail.com>
2022-04-14 00:16:55 +0300

    task_2

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,16 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+const ingredientsEl = document.querySelector("#ingredients");
+const itemEl = ingredients.map((arry) => {
+  const item = document.createElement("li");
+  item.textContent = arry;
+  item.classList.add("item");
+  return item;
+});
+ingredientsEl.append(...itemEl);
