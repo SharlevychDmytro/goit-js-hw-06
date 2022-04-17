53cd099d52a0166bdb577055974f8d2ed2a79a34
Sharlevych Dmytro <sharlevychdmytro@gmail.com>
2022-04-17 21:26:21 +0300

    fix bug task_6

--- js/task-06.js
+++ js/task-06.js
@@ -2,10 +2,10 @@ const refs = {
   inputEl: document.querySelector("#validation-input"),
 };
 refs.inputEl.addEventListener("blur", onBlurInput);
-const inputDataLength = refs.inputEl.getAttribute("data-length");
+const inputDataLength = Number(refs.inputEl.getAttribute("data-length"));
 function onBlurInput(input) {
   const inputValueLength = input.currentTarget.value.length;
-  if (inputValueLength < inputDataLength) {
+  if (inputValueLength !== inputDataLength) {
     refs.inputEl.classList.add("invalid");
   } else {
     refs.inputEl.classList.remove("invalid");
