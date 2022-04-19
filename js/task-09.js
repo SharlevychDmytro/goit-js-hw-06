a95774e5c60d1d4a4c0397949f870536646ca160
Sharlevych Dmytro <sharlevychdmytro@gmail.com>
2022-04-19 08:24:25 +0300

    task_9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,14 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+const refs = {
+  buttonEl: document.querySelector(".change-color"),
+  spanEl: document.querySelector(".color"),
+  bodyEl: document.querySelector("body"),
+};
+refs.buttonEl.addEventListener("click", onBtnClick);
+function onBtnClick() {
+  let randomColor = getRandomHexColor();
+  refs.bodyEl.style.backgroundColor = `${randomColor}`;
+  refs.spanEl.textContent = randomColor;
+}
