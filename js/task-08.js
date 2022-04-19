46e2e6545fda85cab215a7dc1623f03d1cc2890c
Sharlevych Dmytro <sharlevychdmytro@gmail.com>
2022-04-19 08:09:00 +0300

    task_08

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,19 @@
-
+const refs = {
+  formEl: document.querySelector(".login-form"),
+};
+refs.formEl.addEventListener("submit", onFormBtn);
+function onFormBtn(event) {
+  event.preventDefault();
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+  if (email.value === "" || password.value === "") {
+    alert("Fill in all fields");
+  }
+  const formData = {
+    email: email.value,
+    password: password.value,
+  };
+  console.log(formData);
+  refs.formEl.reset();
+}
